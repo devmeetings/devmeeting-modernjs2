e21faa6c8d45bf6f84221c72a5394ce690d8054a
Tomusdrw <tomusdrw@gmail.com>
2016-06-11 16:54:05 +0200

    Create React App [Starter]

---
+++ config.js
@@ -0,0 +1,8 @@
+System.config({
+  defaultJSExtensions: true,
+  transpiler: false,
+  map: {
+    "css": "/cdn/systemjs/plugin-css/css-0.1.20.js",
+    "json": "/cdn/systemjs/plugin-json/json.js"
+  }
+});
