dcb6f8f2d55e26cf05a94355b46511c8cc05ac27
Tomusdrw <tomusdrw@gmail.com>
2016-09-03 13:15:24 +0200

    Deferring scripts [Defer]

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-// Wypiszmy element dynamicznie!
-document.writeln('<h1>I am comming from JS!</h1>');
+document.writeln('<h1>I am comming from JS...</h1>');
+//- document.body.innerHTML += '<h1>I am comming from JS...</h1>';
