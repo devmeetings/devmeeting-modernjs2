63f6f31d6f9bc1210a601cc9e60d42d8f863c97e
Tomusdrw <tomusdrw@gmail.com>
2016-09-03 14:24:34 +0200

    Atrybuty i propertiesy [Properties]

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-//2/ Tworzymy nowy element h1
 var $h1= document.createElement('h1');
+// Modyfikujemy zawartość tekstową
 $h1.textContent = 'Created in <strong>JS</strong>';
 
-//2/ Znajdujemy element body i dodajemy mu dziecko
+// Modyfikujemy atrybut class lub zmieniamy propsa className
+$h1.setAttribute('class', 'color');
+$h1.className = 'color';
+
+// Wygodniejsze API
+$h1.classList.add('shadow');
+
+// 2/ Dodajemy do drzewa DOM
 var $body = document.querySelector('body');
 $body.appendChild($h1);
 
