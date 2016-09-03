9c8ed55028373256980d80bd89257119d212eba8
Tomusdrw <tomusdrw@gmail.com>
2016-09-03 17:10:16 +0200

    Każda funkcja ma własny kontekst wywołania [Zgubiony kontekst]

--- src/index.js
+++ src/index.js
@@ -5,18 +5,26 @@ const Greeter = {
   greet () {
     return 'Hello ' + this.who;
   },
-  
-  //7/ Dodajemy funkcję, która potrafi wyrenderować greet.
+
   renderGreet () {
     const result = this.greet();
 
     const $greet = document.querySelector('.greeter');
     $greet.innerHTML = result;
     console.log(result);
+  },
+
+  addListener () {
+    const $btn = document.querySelector('.greeter__trigger');
+    // To nie zadziała - ten sam problem co wcześniej.
+    //- $btn.addEventListener('click', this.renderGreet);
+
+    //3/ Spróbujmy w ten sposób:
+    $btn.addEventListener('click', function () {
+      console.log(this);
+      this.renderGreet();
+    });
   }
 };
 
-// Dodajemy listener do przycisku
-const $btn = document.querySelector('.greeter__trigger');
-// Będziemy po prostu wołać `Greeter.renderGreet`
-$btn.addEventListener('click', Greeter.renderGreet);
+Greeter.addListener();
