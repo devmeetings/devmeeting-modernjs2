eef68ad102b2566cecf88fee60be696617d5ace9
Tomusdrw <tomusdrw@gmail.com>
2016-09-03 18:24:30 +0200

    Renderowanie zadań z użyciem pętli [Func - Intro]

--- src/index.js
+++ src/index.js
@@ -1,45 +1,37 @@
 'use strict';
 
-//5/ Tworzymy klasę bazową Animal
-class Animal {
-  sayName () {
-    return this.name;
+//27/ 3. Klasa widoku, podobna do tej z plikacji do aktywności.
+class View {
+
+  //3/ 4. W konstruktorze przyjmujemy selektor
+  constructor (selector) {
+    this.$el = document.querySelector(selector);
+  }
+
+  //5/ 5. Renderowanie pojedynczego zadania
+  renderTask (task) {
+    const $el = document.createElement('li');
+    $el.textContent = task;
+    return $el;
+  }
+
+  //6/ Pętla, w której renderujemy zadania
+  render (tasks) {
+    for (let idx in tasks) {
+      let $task = this.renderTask(tasks[idx]);
+      this.$el.appendChild($task);
+    }
   }
 }
 
-//6/ Standardowe dziedziczenie 
-class Dog extends Animal {
-  constructor (name) {
-    super();
-    this.name = name;
-  }
-}
+//7/ 1. Mamy listę zadań, którą będziemy renderować.
+const tasks = [
+  'Learn JavaScript',
+  'Learn ES6',
+  'Learn React',
+  '???',
+  'Profit!'
+];
 
-//6/ Klasa Kota też dziedziczy po Animal
-class Cat extends Animal {
-  constructor(name) {
-    super();
-    console.log(this);
-    this.name = name;
-  }
-}
-
-//4/ Stwórzmy instancję kota i psa
-const cat1 = new Cat('Tomek');
-const dog1 = new Dog('Marek');
-console.dir(cat1);
-console.dir(dog1);
-
-//11/ Sprawdźmy kilka własności
-const result = `
-  Cat's Name: ${cat1.sayName()}
-  Dog's Name: ${dog1.sayName()}
-
-  Is cat1 a Cat?: ${cat1 instanceof Cat}
-  Is dog1 a Dog?: ${dog1 instanceof Dog}
-  
-  Is cat1 an Animal?: ${cat1 instanceof Animal}
-
-  Is cat1 a Dog?: ${cat1 instanceof Dog}
-`;
-document.querySelector('.greeter').innerHTML = result;
+// 2. Tworzymy nowy widok i przekazujemy taski
+new View('.todos').render(tasks);
