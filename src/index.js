5bcb6d0d9b57caa5b163348e27c094479595e3a3
Tomusdrw <tomusdrw@gmail.com>
2016-09-03 17:07:05 +0200

    Listeners

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-// 3. Zachowanie tego kodu jest zależne trybu strict
-//- 'use strict';
+'use strict';
 
 const Greeter = {
   who: 'Tomasz',
   greet () {
     return 'Hello ' + this.who;
+  },
+  
+  //7/ Dodajemy funkcję, która potrafi wyrenderować greet.
+  renderGreet () {
+    const result = this.greet();
+
+    const $greet = document.querySelector('.greeter');
+    $greet.innerHTML = result;
+    console.log(result);
   }
-}
+};
 
-// 1. Zapisujemy funkcję do zmiennej...
-const justGreet = Greeter.greet;
-
-// 2. ... i wywołujemy - nie ma nic przed kropką!
-const result = justGreet();
-
-const $greet = document.querySelector('.greeter');
-$greet.innerHTML = result;
-console.log(result);
+// Dodajemy listener do przycisku
+const $btn = document.querySelector('.greeter__trigger');
+// Będziemy po prostu wołać `Greeter.renderGreet`
+$btn.addEventListener('click', Greeter.renderGreet);
