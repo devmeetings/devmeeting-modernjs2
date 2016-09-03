52f5d7aadb452322a3b93b46be5cf8b6c6b97b44
Tomusdrw <tomusdrw@gmail.com>
2016-09-04 01:45:12 +0200

    Obsługa klas CSS [classnames]

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ System.config({
     "css": "/cdn/systemjs/plugin-css/css-0.1.20.js",
     "json": "/cdn/systemjs/plugin-json/json.js",
     "react": "/cdn/react/15.1.0/react.js",
-    "react-dom": "/cdn/react/15.1.0/react-dom.js"
+    "react-dom": "/cdn/react/15.1.0/react-dom.js",
+    "classnames": "/cdn/classnames/2.2.5/classnames.js"
   }
 });
