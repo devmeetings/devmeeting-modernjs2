e7e54e04517b1658da9eb3a536a3bcb61c1aac04
Tomusdrw <tomusdrw@gmail.com>
2016-09-03 14:10:10 +0200

    Tworzenie elementów DOM [createElement]

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-// querySelector pozwala używać selektorów CSS do odszukiwania elementow
-var $elem = document.querySelector('h1');
-// przez innerHTML możemy modyfikować ich zawartość
-$elem.innerHTML = 'Your JS Activities';
+//2/ Tworzymy nowy element h1
+var $h1= document.createElement('h1');
+$h1.textContent = 'Created in <strong>JS</strong>';
+
+//2/ Znajdujemy element body i dodajemy mu dziecko
+var $body = document.querySelector('body');
+$body.appendChild($h1);
+
