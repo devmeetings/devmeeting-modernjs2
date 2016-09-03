0b450dbd474fbf4efad423202948c54def90730e
Tomusdrw <tomusdrw@gmail.com>
2016-09-03 17:40:08 +0200

    Każda z instancji obiektu ma swój własny zestaw funkcji [Flyweight]

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
 'use strict';
 
-//3/ Greeter nie ma teraz funkcji greet
 function Greeter(who) {
   this.who = who;
+  this.greet = function () {
+    return `Hello ${this.who}`;
+  };
 };
-
-//3/ Tworzymy funkcję niezależnie i korzystamy z this
-function greet() {
-  return `Hello ${this.who}`;
-}
 
 const greeter1 = new Greeter('Tomasz');
 const greeter2 = new Greeter('Marek');
 
-// Wywołajmy funkcję `greet` w kontekście obiektu `greeter1`
-let result = greet.call(greeter1);
+//2/ Zobaczmy jak obiekty wyglądają w konsoli
+console.dir(greeter1);
+console.dir(greeter2);
 
-// Możemy też użyć apply
-result += ' ' + greet.apply(greeter2);
-
-// bind utworzy nową funkcje, która ma związany kontekst
-const greetMe = greet.bind({ who: 'Anonymous' });
-
-// Możemy ją potem wywołac "bez kontekstu"
-result += ' ' + greetMe();
+// I porównajmy funkcje greet.
+const result = 'Różne: ' + (greeter1.greet !== greeter2.greet);
 
 document.querySelector('.greeter').innerHTML = result;
