6cd085972fc149549c88ef81d9b9c9eb1549b1de
Tomusdrw <tomusdrw@gmail.com>
2016-09-03 17:19:40 +0200

    Tworzenie nowych obiektów [new]

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
 'use strict';
 
-const Greeter = {
-  who: 'Tomasz',
-  greet () {
-    return 'Hello ' + this.who;
-  },
-
-  renderGreet () {
-    const result = this.greet();
-
-    const $greet = document.querySelector('.greeter');
-    $greet.innerHTML = result;
-    console.log(result);
-  },
-
-  addListener () {
-    const $btn = document.querySelector('.greeter__trigger');
-    // To nie zadziała - ten sam problem co wcześniej.
-    //- $btn.addEventListener('click', this.renderGreet);
-
-    //3/ Spróbujmy w ten sposób:
-    $btn.addEventListener('click', function () {
-      console.log(this);
-      this.renderGreet();
-    });
-  }
+//9/ Stwórzmy sobie funkcję, która przypisuje coś do this.
+function Greeter(who) {
+  this.who = who;
+  this.greet = function () {
+    return `Hello ${this.who}`;
+  };
 };
 
-Greeter.addListener();
+//2/ Stwórzmy dwie instancje.
+const greeter1 = new Greeter('Tomasz');
+const greeter2 = new Greeter('Marek');
+
+// I wywołajmy je.
+const result = greeter1.greet() + ' ' + greeter2.greet();
+
+//  `new` tworzy nowy obiekt, i wywołuje
+//- funkcję `Greeter` w kontekście tego obiektu.
+
+document.querySelector('.greeter').innerHTML = result;
