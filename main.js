aee6f15cabedf5fb5818163b8e13194dda44a968
Tomusdrw <tomusdrw@gmail.com>
2016-09-03 14:44:02 +0200

    Obiekty w JS [Obiekty]

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
-// 6/ Tworzymy obrazek i nadajemy mu odpowiednie propsy
+// 6/ Obiekt w JS to `Object := HashMap<String, Object> | Primitive`
+var activity = {
+  id: 3,
+  alt: 'Bicycle',
+  name: 'Cycling',
+  timeSpent: 120,
+};
+
 var $img = document.createElement('img');
 $img.className = 'activity__img';
 $img.height = 250;
 $img.width = 250;
-$img.alt = 'Bicycle';
-$img.src = 'https://xplatform.org/ext/lorempixel/250/250/sports/3/';
+// Do propertiesów obiektu odwołujemy się po kropce
+$img.alt = activity.alt;
+$img.src = 'https://xplatform.org/ext/lorempixel/250/250/sports/' + activity.id;
 
-// 3/ Nazwę aktywności
 var $title = document.createElement('h3');
 $title.className = 'activity__name';
-$title.textContent = 'Cycling';
+// Lub równoważnie przez []
+$title.textContent = activity['name'];
 
-// 3/ Opis
 var $desc = document.createElement('p');
 $desc.className = 'activity__description';
-$desc.innerHTML = 'Time spent: <strong>120 min</strong>';
+$desc.innerHTML = 'Time spent: <strong>' + activity.timeSpent + '</strong>';
 
-// 3/ Przycisk
 var $button = document.createElement('button');
 $button.className = 'activity__button--paused';
 $button.innerHTML = '&#9654; Start';
 
-//6/ Składamy w całość...
 var $activity = document.createElement('div');
 $activity.className = 'activity';
 $activity.appendChild($img);
@@ -29,7 +34,6 @@ $activity.appendChild($title);
 $activity.appendChild($desc);
 $activity.appendChild($button);
 
-// 2/ ...i dodajemy do drzewa DOM
 var $activities = document.querySelector('.activities');
 $activities.innerHTML = '';
 $activities.appendChild($activity);
