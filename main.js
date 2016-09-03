04917837b86d2b9e004236f8a73e0c516bd83416
Tomusdrw <tomusdrw@gmail.com>
2016-09-03 14:33:08 +0200

    Generowanie DOM z JS - Single Page Application [SPA]

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
-var $h1= document.createElement('h1');
-// Modyfikujemy zawartość tekstową
-$h1.textContent = 'Created in <strong>JS</strong>';
+// 6/ Tworzymy obrazek i nadajemy mu odpowiednie propsy
+var $img = document.createElement('img');
+$img.className = 'activity__img';
+$img.height = 250;
+$img.width = 250;
+$img.alt = 'Bicycle';
+$img.src = 'https://xplatform.org/ext/lorempixel/250/250/sports/3/';
 
-// Modyfikujemy atrybut class lub zmieniamy propsa className
-$h1.setAttribute('class', 'color');
-$h1.className = 'color';
+// 3/ Nazwę aktywności
+var $title = document.createElement('h3');
+$title.className = 'activity__name';
+$title.textContent = 'Cycling';
 
-// Wygodniejsze API
-$h1.classList.add('shadow');
+// 3/ Opis
+var $desc = document.createElement('p');
+$desc.className = 'activity__description';
+$desc.innerHTML = 'Time spent: <strong>120 min</strong>';
 
-// 2/ Dodajemy do drzewa DOM
-var $body = document.querySelector('body');
-$body.appendChild($h1);
+// 3/ Przycisk
+var $button = document.createElement('button');
+$button.className = 'activity__button--paused';
+$button.innerHTML = '&#9654; Start';
 
+//6/ Składamy w całość...
+var $activity = document.createElement('div');
+$activity.className = 'activity';
+$activity.appendChild($img);
+$activity.appendChild($title);
+$activity.appendChild($desc);
+$activity.appendChild($button);
+
+// 2/ ...i dodajemy do drzewa DOM
+var $activities = document.querySelector('.activities');
+$activities.innerHTML = '';
+$activities.appendChild($activity);
