af2f9e3576221fa86cdd585d54afaf6fe8a846d0
Tomusdrw <tomusdrw@gmail.com>
2016-09-03 18:31:10 +0200

    Map

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
 'use strict';
 
-//27/ 3. Klasa widoku, podobna do tej z plikacji do aktywności.
 class View {
 
-  //3/ 4. W konstruktorze przyjmujemy selektor
   constructor (selector) {
     this.$el = document.querySelector(selector);
   }
 
-  //5/ 5. Renderowanie pojedynczego zadania
   renderTask (task) {
     const $el = document.createElement('li');
     $el.textContent = task;
     return $el;
   }
 
-  //6/ Pętla, w której renderujemy zadania
   render (tasks) {
-    for (let idx in tasks) {
-      let $task = this.renderTask(tasks[idx]);
-      this.$el.appendChild($task);
-    }
+    //5/ Zamiast iterować w pętli używamy `map`
+    tasks
+      .map(function (task) {
+        return this.renderTask(task);
+      }, this)
+      .map($task => this.$el.appendChild($task));
   }
 }
 
-//7/ 1. Mamy listę zadań, którą będziemy renderować.
 const tasks = [
   'Learn JavaScript',
   'Learn ES6',
@@ -33,5 +30,4 @@ const tasks = [
   'Profit!'
 ];
 
-// 2. Tworzymy nowy widok i przekazujemy taski
 new View('.todos').render(tasks);
