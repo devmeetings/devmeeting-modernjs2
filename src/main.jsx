28f588644b12b370bc250f598c34dd46b9486e49
Tomusdrw <tomusdrw@gmail.com>
2016-09-04 02:29:23 +0200

    JSX Demystified [JSX]

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,32 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-// 1. Komponent może być też klasą rozszerzającą `React.Component`...
 class App extends React.Component {
 
-  //3/ 4. Dzięki stage-0 możemy definiować pola bez użycia konstruktura
   state = {
     now: 0
   };
 
-  //6/ 5. Uaktualniamy stan cyklicznie
   componentDidMount() {
     setInterval(() => {
       this.setState({ now: Date.now() });
     }, 100);
   }
 
-  //12/ 2. ...zawierającą funkcję `render`
+  //8/ JSX to po prostu przyjemniejsza składnia na poniższy zapis
   render () {
-    // 3. Wartość czasu odczytujemy ze "stanu" komponentu
     const now = this.state.now;
 
-    return (
-      <div>
-        <h1>Hello React!</h1>
-        <input type='text' placeholder='some input' />
-        <p>Now: {now}</p>
-      </div>
+    return React.createElement('div', null,
+      React.createElement('h1', null, 'Hello React!'),
+      React.createElement('input', {type: 'text', placeholder: 'some input'}),
+      React.createElement('p', null, `Now: ${now}`),
     );
   }
 }
 
-render(<App />, document.querySelector('#app'));
+render(
+  React.createElement(App, null),
+  document.querySelector('#app')
+);
 
