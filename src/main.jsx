3b21e999a4b53bd9f6e79fae18d3c82d27a73764
Tomusdrw <tomusdrw@gmail.com>
2016-09-04 02:28:55 +0200

    Stateful Component [State]

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,35 @@
-// Importujemy React (potrzebne do przetwarzania JSX).
 import React from 'react';
-// React może być używany także poza przeglądarką, ale my potrzebujemy `react-dom`.
 import {render} from 'react-dom';
 
-//5/ Najprostszym komponentem jest po prostu funkcja.
-function App() {
-  const now = new Date().getTime();
-  return (
-    <div>
-      <h1>Hello React!</h1>
-      <input type='text' placeholder='some input' />
-      <p>Now: {now}</p>
-    </div>
-  );
+// 1. Komponent może być też klasą rozszerzającą `React.Component`...
+class App extends React.Component {
+
+  //3/ 4. Dzięki stage-0 możemy definiować pola bez użycia konstruktura
+  state = {
+    now: 0
+  };
+
+  //6/ 5. Uaktualniamy stan cyklicznie
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ now: Date.now() });
+    }, 100);
+  }
+
+  //12/ 2. ...zawierającą funkcję `render`
+  render () {
+    // 3. Wartość czasu odczytujemy ze "stanu" komponentu
+    const now = this.state.now;
+
+    return (
+      <div>
+        <h1>Hello React!</h1>
+        <input type='text' placeholder='some input' />
+        <p>Now: {now}</p>
+      </div>
+    );
+  }
 }
 
-// Musimy określić gdzie wyrenderujemy nasz pierwszy komponent.
 render(<App />, document.querySelector('#app'));
+
