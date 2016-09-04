a2fb9c6bc4bb4bc42d18b58037085963942341f1
Tomusdrw <tomusdrw@gmail.com>
2016-09-04 02:30:52 +0200

    Wydzielamy komponent Task [Osobny komponent]

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+// Importujemy komponent z osobnego pliku
+import Task from './components/Task';
+
 class Tasks extends React.Component {
 
   state = {
@@ -9,18 +12,15 @@ class Tasks extends React.Component {
   };
 
   componentDidMount () {
-    //5/ Pobieramy zadania na starcie
     fetch('data/tasks.json')
       .then(res => res.json())
       .then(tasks => this.setState({
         tasks
       }));
 
-    // Musimy odświeżać aktualny czas...
     this.interval = setInterval(() => this.setState({ now: Date.now() }), 1000);
   }
 
-  //3/ ... i pamiętać o tym, żeby po sobie posprzątać
   componentWillUnMount () {
     clearInterval(this.interval);
   }
@@ -31,24 +31,14 @@ class Tasks extends React.Component {
     return (
       <div>
         <h1>Hello React!</h1>
-        {/* Renderowanie pojedynczego zadania przenosimy do osobnej funkcji */}
-        { tasks.map((task, idx) => this.renderTask(task, now, idx)) }
+        {/*4/ Korzystamy z zaimportowanego komponentu w JSX */}
+        { tasks.map((task, idx) =>(
+          <Task task={task} now={now} key={idx} />
+        )) }
       </div>
     );
   }
 
-  renderTask (task, now, idx) {
-    const isActive = !task.finished;
-    const time = ((task.finished || now) - task.started) / 1000 / 60;
-    const name = task.name; 
-
-    return (
-      <div className={`task ${isActive ? 'active' : ''}`} key={idx}>
-        <span>{name} </span>
-        <span>{time.toFixed(1)} min</span>
-      </div>   
-    );
-  }
 }
 
 render(<Tasks />, document.querySelector('#app'));
