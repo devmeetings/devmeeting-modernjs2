0d4c4ec2078c2950840ca4672be8c7ab19203656
Tomusdrw <tomusdrw@gmail.com>
2016-09-04 02:30:26 +0200

    Aplikacja z zadaniami [Tasks App]

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,55 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-class App extends React.Component {
+import tasks from '../data/tasks.json';
 
-  state = { now: 0 };
+class Tasks extends React.Component {
 
-  componentDidMount() {
-    setInterval(() => {
-      // Wywołanie `setState` zmusza reacta do wywłoania `render`
-      this.setState({ now: Date.now() });
-    }, 100);
+  state = {
+    now: 0,
+    tasks: []
+  };
+
+  componentDidMount () {
+    //5/ Pobieramy zadania na starcie
+    setTimeout(() => {
+      this.setState({ tasks });
+    }, 500);
+
+    // Musimy odświeżać aktualny czas...
+    this.interval = setInterval(() => this.setState({ now: Date.now() }), 1000);
   }
 
-  // Funkcja `render` powinna być "lekka" bo będzie często wywoływana
+  //3/ ... i pamiętać o tym, żeby po sobie posprzątać
+  componentWillUnMount () {
+    clearInterval(this.interval);
+  }
+
   render () {
-    const now = this.state.now;
+    const { now, tasks } = this.state;
 
     return (
       <div>
         <h1>Hello React!</h1>
-        <input type='text' placeholder='some input' />
-        <p>Now {now}</p>
+        {/* Renderowanie pojedynczego zadania przenosimy do osobnej funkcji */}
+        { tasks.map((task, idx) => this.renderTask(task, now, idx)) }
       </div>
     );
   }
 
-  /*
-   * A tak wygląda reprezentacja tego komponentu:
-   * {
-   *   type: 'div',
-   *   props: null,
-   *   children: [{ type: 'h1',...}, ...]
-   * }
-   */
+  renderTask (task, now, idx) {
+    const isActive = !task.finished;
+    const time = ((task.finished || now) - task.started) / 1000 / 60;
+    const name = task.name; 
+
+    return (
+      <div className={`task ${isActive ? 'active' : ''}`} key={idx}>
+        <span>{name} </span>
+        <span>{time.toFixed(1)} min</span>
+      </div>   
+    );
+  }
 }
 
-render(<App />, document.querySelector('#app'));
+render(<Tasks />, document.querySelector('#app'));
+
