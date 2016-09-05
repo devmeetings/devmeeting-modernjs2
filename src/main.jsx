d4e771cc48f1a2ec1248550e021990872ba7d808
Tomusdrw <tomusdrw@gmail.com>
2016-09-05 23:18:24 +0200

    Komponent bez Reacta [No-React]

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,23 @@
-// Na razie używamy czytego JS żeby wyświetlić na stronie cokolwiek.
-document.querySelector('#app').innerHTML = '<h1>Hello React!</h1>';
+//11/ Definicja naszego komponentu
+function App() {
+  // Aktualny stan
+  const now = new Date().getTime();
+
+  //5/ Definicja widoku
+  return `<div>
+    <h1>Hello React!</h1>
+    <input type="text" placeholder="some input" />
+    <p>Now: ${now}</p>
+  </div>`;
+}
+
+//3/ Musimy re-renderować komponent co jakiś czas
+setInterval(() => {
+  render(App, document.querySelector('#app'));
+}, 2000);
+
+//3/ Pomocnicza wstawiająca komponent do drzewa DOM
+function render(component, $app) {
+  $app.innerHTML = component();
+}
+
